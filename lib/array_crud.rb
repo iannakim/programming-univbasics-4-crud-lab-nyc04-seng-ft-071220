0b3361411dfe47f24279691f5379a55c3a15a9df
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-16 15:41:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  my_array = ["wow", "strawberry", "banana", "pear", "apple", "arrays!"]
+  my_array[4] "totally"
 end
