09181c756ef4af551ec6e3b5945c2bd6b2ab8743
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-16 15:36:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  my_array = ["wow", 'strawberry", "banana", "pear", "apple", "arrays!"]
-  my_array.shift
+  my_array = ["wow", strawberry", "banana", "pear", "apple", "arrays!"]
 end
 
 def retrieve_element_from_index(array, index_number)
